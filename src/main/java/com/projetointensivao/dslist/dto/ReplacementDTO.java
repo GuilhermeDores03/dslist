88ec7c701204438cfeef489d6b5114db07361e4a
Guilherme Dores <guilhermezdores@gmail.com>
2025-01-24 10:26:57 -0300

    Move games on list

---
+++ src/main/java/com/projetointensivao/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,21 @@
+package com.projetointensivao.dslist.dto;
+
+public class ReplacementDTO {
+
+	private Integer sourceIndex;
+	private Integer destinationIndex;
+	
+	//getters setters
+	public Integer getSourceIndex() {
+		return sourceIndex;
+	}
+	public void setSourceIndex(Integer sourceIndex) {
+		this.sourceIndex = sourceIndex;
+	}
+	public Integer getDestinationIndex() {
+		return destinationIndex;
+	}
+	public void setDestinationIndex(Integer destinationIndex) {
+		this.destinationIndex = destinationIndex;
+	}	
+}
